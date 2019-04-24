607139fc327e38099f740e0c53a19106afaba97e
alaatarek95 <alaatarek95@gmail.com>
2019-04-24 06:49:14 +0200

    create migration files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('username')->unique();
+            $table->string('phone')->nullable();
+            $table->boolean('is_active');
+            $table->integer('national_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
