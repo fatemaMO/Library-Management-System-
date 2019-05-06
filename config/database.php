d53a407cc5acab25259a207dc73eb8d6334d5a85
aml <aml.fci@gmail.com>
2019-05-06 15:38:55 +0200

    fix conflicts

--- config/database.php
+++ config/database.php
@@ -44,11 +44,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'sql2.freemysqlhosting.net'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'sql2290702'),
-            'username' => env('DB_USERNAME', 'sql2290702'),
-            'password' => env('DB_PASSWORD', 'aJ5%qL8!'),
+            'database' => env('DB_DATABASE', 'libraryiti'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
